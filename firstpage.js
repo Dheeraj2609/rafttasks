a9e1be16949c7b9b966c1ae1344fae64d068ab92
Dheeraj P <67047093+Dheeraj2609@users.noreply.github.com>
2021-05-25 12:56:01 +0530

    Add files via upload

---
+++ firstpage.js
@@ -0,0 +1,29 @@
+function details() {
+	document.querySelector(".footerContents").style.display = "none";     //To show the details of
+	document.querySelector(".footerHideContents").style.display = "block";  // terms and conditions
+}
+
+function hideDetails() {
+	document.querySelector(".footerContents").style.display = "block";    //to hide the details of 
+	document.querySelector(".footerHideContents").style.display = "none"; // terms and conditions
+}
+/*
+let content1={
+	image: url("d:/max bupa/bitmap.png"),
+	h1: "HEALTHY EATING AT OUR CAFETERIA",
+	para: "Lorem Ipsum Deoore Desstysh hhunddgernjj hunnamns, huundft lorem"
+};
+
+let content2={
+	image1: url("d:/max bupa/bitmap1.png"),
+	image2: url("d:/max bupa/bitmap2.png"),
+	image3: url("d:/max bupa/bitmap3.png"),
+	image4: url("d:/max bupa/bitmap4.png")
+};
+
+function tab() {
+	var element = document.getElementsByClassName("outineOfTab1")[0].style.backgroundImage;
+	if(element == "url(bitmap.png)") { 
+   	element.style.backgroundImage = "url(/max bupa/group-35.png)"; 
+	}
+}    */
