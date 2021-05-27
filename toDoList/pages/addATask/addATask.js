a0dd847abfb1e3c8f21b7defb8cd67af52cf9984
Dheeraj <dheeraj.p@moonraft.com>
2021-05-27 16:51:03 +0530

    organised

--- toDoList/pages/addATask/addATask.js
+++ toDoList/pages/addATask/addATask.js
@@ -1,16 +1,18 @@
 if (typeof Storage !== "undefined") {
-  document.querySelector(".add__task__save").onclick = function () {
-    var tasks = document.querySelector(".add__task__input").value;
-    var descriptions = document.querySelector(".task__describe").value;
-    var dates = document.querySelector(".task__date").value;
-    var times = document.querySelector(".task__time").value;
-    var radios = document.querySelector('input[name="task"]:checked').value;
+  document.querySelector(".saveBtn").addEventListener("click", setFunction);
+ 
+  function setFunction() {
+    var tasks = document.querySelector(".taskInfo__input").value;
+    var descriptions = document.querySelector(".taskInfo__des").value;
+    var dates = document.querySelector(".taskDate").value;
+    var times = document.querySelector(".taskTime").value;
+    var set_plateform = document.querySelector('input[name="task"]:checked').value;
     localStorage.setItem("task", tasks);
     localStorage.setItem("description", descriptions);
     localStorage.setItem("date", dates);
     localStorage.setItem("time", times);
-    localStorage.setItem("radio", radios);
-  };
+    localStorage.setItem("radio", set_plateform);
+  }
 } else {
   document.querySelector(".result").innerHTML = "Browser does not support";
 }
