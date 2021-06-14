ecb20666d0ed38d9719d1a67ac227d76816839c1
Dheeraj <dheeraj.p@moonraft.com>
2021-06-14 11:30:09 +0530

    updated ToDoList

--- toDoList/pages/toDoList/new.js
+++ toDoList/pages/toDoList/new.js
@@ -1,119 +1,82 @@
 if (typeof Storage !== "undefined") { //Checking browser support
   var get_info = JSON.parse(localStorage.getItem("userval"));
-  var selPurpose = document.querySelector(".taskCntr__drop").value;
+ 
+    if(localStorage.getItem("temp") == undefined){
+        var Thelist = [];
+        localStorage.setItem("temp", JSON.stringify(Thelist));
+      }
+      var Thelist_parse = localStorage.getItem("temp");
+      var Thelist = JSON.parse(Thelist_parse);
+      Thelist.push(get_info);
+      localStorage.setItem("temp",JSON.stringify(Thelist));
+      console.log(Thelist);
   console.log(get_info);
+  var user_date = new Date(get_info[2]);
+  var today_date = new Date(); //Setting today's date to the variable
+  var times = get_info[3].split(":"); //Splitting time with :
+  var user_full_date = new Date( // setting variable with the date format local storage data
+    user_date.getFullYear(),
+    user_date.getMonth(),
+    user_date.getDate(),
+    times[0],
+    times[1]
+  );
+  var ms = user_full_date - today_date; //Getting difference between current and given date and set it to variable
+
+
+  /*
+  var i;
+  var listinfo = [0];
+  for(i in listinfo){
+    for(i in get_info){
+      listinfo.push(get_info[i]);
+    }
+    
+  }
+  console.log(listinfo);*/
+
+
   window.addEventListener("load", onLoad); //onload event
   document.querySelector(".taskCntr__drop").addEventListener("change", onChange); //onchange event
-  if(!get_info) {
-        document.querySelector(".taskCntr").style.display = "none"; //if task is not filled task will never displayed
-      } 
+  document.querySelector(".detlInfo__head").innerHTML = get_info[0]; //Setting task head and description
+  document.querySelector(".detlInfo__info").innerHTML = get_info[1]; //from local storage
+
+  days = Math.floor(ms / (24 * 60 * 60 * 1000)); //calculating diffenrce in days hours and minutes
+  daysms = ms % (24 * 60 * 60 * 1000);
+  hours = Math.floor(daysms / (60 * 60 * 1000));
+  hoursms = ms % (60 * 60 * 1000);
+  minutes = Math.floor(hoursms / (60 * 1000));
+  if (days > 0 || hours > 0 || minutes > 0) {
+    // checking and Displaying the differnce in dates
+    document.querySelector(".taskTime__text").innerHTML = days + " days " + hours + " hrs " + minutes + " mins to go";
+  } else if (days <= 0 || hours <= 0 || minutes <= 0) {
+    //checking whether date is less than current date
+    document.querySelector(".taskTime__text").innerHTML = "OVERDUE!!!"; //If true displaying OVERDUE!!!
+  }
+
   function onLoad() {
-    document.querySelector(".taskCntr__drop").value = "personal";
-    selPurpose = document.querySelector(".taskCntr__drop").value;
-    var temp1  = get_info.filter(function (loadcheck) {
-      return loadcheck.taskpurpose == selPurpose;
-    });
-    console.log(selPurpose);
-    console.log(temp1);
-    for (i=0;i<temp1.length;i++){
-        var user_date = new Date(temp1[i].taskdate);
-        var today_date = new Date();  //Setting today's date to the variable
-        var times = temp1[i].tasktime.split(":");  //Splitting time with :
-        var user_full_date = new Date(  // setting variable with the date format local storage data
-          user_date.getFullYear(),
-          user_date.getMonth(),
-          user_date.getDate(),
-          times[0],
-          times[1]
-        );
-        var time_string;
-        var ms = user_full_date - today_date; //Getting difference between current and given date and set it to variable
-        days = Math.floor(ms / (24 * 60 * 60 * 1000)); //calculating diffenrce in days hours and minutes
-        daysms = ms % (24 * 60 * 60 * 1000);
-        hours = Math.floor(daysms / (60 * 60 * 1000));
-        hoursms = ms % (60 * 60 * 1000);
-        minutes = Math.floor(hoursms / (60 * 1000));
-        if (days > 0 || hours > 0 || minutes > 0) {// checking and Displaying the differnce in dates
-          time_string = days + " days " + hours + " hrs " + minutes + " mins to go";
-        } 
-        else if (days <= 0 || hours <= 0 || minutes <= 0) { //checking whether date is less than current date
-          time_string = "OVERDUE!!!"; //If true displaying OVERDUE!!!
-        }
-        console.log(time_string);
-      
-    var x = `<div class="infoDetail">
-    <div class="infoStatus">
-      <span class="infoStatus__marker"> T </span>
-      <img src="../../assets/images/group.png" alt="status" />
-    </div>
-    <div class="taskInfo">
-      <div class="detlInfo">
-        <span class="detlInfo__head">` + temp1[i].taskName + `</span>
-        <p class="detlInfo__info">` + temp1[i].taskdesc + `</p>
-      </div>
-      <div class="taskTime">
-        <span class="taskTime__text">` + time_string + `</span>
-      </div>
-    </div>
-  </div>`
-      var y = document.querySelector(".detailCntr__info").innerHTML;
-      document.querySelector(".detailCntr__info").innerHTML = x+y;
-    
-    }
+    // onload function
+    document.querySelector(".taskCntr__drop").value = get_info[4];
+    if (get_info[4] != document.querySelector(".taskCntr__drop").value) {
+      document.querySelector(".detailCntr__info").style.display = "none";
+    } else document.querySelector(".detailCntr__info").style.display = "block";
   }
-  function onChange () {
-        selPurpose = document.querySelector(".taskCntr__drop").value;
-        var temp = get_info.filter(function (check) {
-                return check.taskpurpose == selPurpose;
-            });
-     console.log(temp);
-     document.querySelector(".detailCntr__info").innerHTML = "";
-     for (i=0;i<temp.length;i++){
-      var user_date = new Date(temp[i].taskdate);
-      var today_date = new Date();  //Setting today's date to the variable
-      var times = temp[i].tasktime.split(":");  //Splitting time with :
-      var user_full_date = new Date(  // setting variable with the date format local storage data
-        user_date.getFullYear(),
-        user_date.getMonth(),
-        user_date.getDate(),
-        times[0],
-        times[1]
-      );
-      var time_string;
-      var ms = user_full_date - today_date; //Getting difference between current and given date and set it to variable
-      days = Math.floor(ms / (24 * 60 * 60 * 1000)); //calculating diffenrce in days hours and minutes
-      daysms = ms % (24 * 60 * 60 * 1000);
-      hours = Math.floor(daysms / (60 * 60 * 1000));
-      hoursms = ms % (60 * 60 * 1000);
-      minutes = Math.floor(hoursms / (60 * 1000));
-      if (days > 0 || hours > 0 || minutes > 0) {// checking and Displaying the differnce in dates
-        time_string = days + " days " + hours + " hrs " + minutes + " mins to go";
-      } 
-      else if (days <= 0 || hours <= 0 || minutes <= 0) { //checking whether date is less than current date
-        time_string = "OVERDUE!!!"; //If true displaying OVERDUE!!!
-      }
-      console.log(time_string);
-   var x = `<div class="infoDetail">
-  <div class="infoStatus">
-    <span class="infoStatus__marker"> T </span>
-    <img src="../../assets/images/group.png" alt="status" />
-  </div>
-  <div class="taskInfo">
-    <div class="detlInfo">
-      <span class="detlInfo__head">` + temp[i].taskName + `</span>
-      <p class="detlInfo__info">` + temp[i].taskdesc + `</p>
-    </div>
-    <div class="taskTime">
-      <span class="taskTime__text">` + time_string + `</span>
-    </div>
-  </div>
-</div>`
-    var y = document.querySelector(".detailCntr__info").innerHTML;
-    document.querySelector(".detailCntr__info").innerHTML = x+y;
-  
+  function onChange() {
+    //onchange function
+    if (get_info[4] != document.querySelector(".taskCntr__drop").value) {
+      document.querySelector(".detailCntr__info").style.display = "none";
+    } else document.querySelector(".detailCntr__info").style.display = "block";
   }
-  }  
-} 
-else {
+} else {
   alert("Browser does not support"); //alert if the browser doesnot support
+}
+
+if (
+  document.querySelector(".detlInfo__head").innerHTML == "" || //checking whether all the field is filled
+  document.querySelector(".detlInfo__info").innerHTML == "" ||
+  document.querySelector(".taskTime__text").innerHTML == ""
+) {
+  document.querySelector(".taskCntr").style.display = "none"; //if it is not filled task will never displayed
+} else {
+  document.querySelector(".taskCntr").style.display = "block"; //if it is filled task will displayed
 }
